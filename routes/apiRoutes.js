e260ddef38e345f0494def1b8fd5750fd477df59
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-12-16 10:28:07 -0800

    Updated apiRoutes for POST and DELETE requests to update the stored notes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,31 @@
-const storedNotes = require("../db/store");
+let store = require("../db/store");
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
-        res.json(storedNotes);
-      });
+        res.json(store.savedNotes);
+    });
 
+    
     app.post("/api/notes", function(req,res) {
-        storedNotes.push(req.body);
+        req.body.id = (store.savedNotes.length + 1).toString();
+        store.savedNotes.push(req.body);
+        store.updateJsonFile(store.savedNotes);
+        res.json(store.savedNotes);
     })
+
+
+    app.delete("/api/notes/:id", function(req, res) {
+        var chosen = req.params.id;
+      
+        for (var i = 0; i < store.savedNotes.length; i++) {
+            
+          if (chosen === store.savedNotes[i].id) {
+            store.savedNotes = store.savedNotes.filter(({ id }) => id !== req.params.id);
+            store.updateJsonFile(store.savedNotes);
+            res.json(store.savedNotes);
+          }
+        }
+      
+        return res.json(false);
+    });
 };

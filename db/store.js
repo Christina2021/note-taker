16dbe3dc1a3c6f923b54694e613309a0820a55b9
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-12-16 10:24:16 -0800

    Updated store.js for a function to re-write db.json when saving/deleting notes.  Added id to test note in db.json to be tested (based on the code listed in the starter file for index.js)

--- db/store.js
+++ db/store.js
@@ -1,12 +1,28 @@
 const jsonfile = require('jsonfile');
+const fs = require("fs");
 const file = './db/db.json';
 
+let savedNotes = [];
+
+//Function for reading the db.json file and adding it to an array
 jsonfile.readFile(file, function (err, obj) {
     if (err) console.error(err);
-    console.dir(obj);
-    console.log(obj.length);
+    // savedNotes = obj;
+    // console.log(savedNotes);
     for (let i = 0; i < obj.length; i++) {
-        console.log(obj[i].title, obj[i].text);
+        savedNotes[i] = { "id": obj[i].id, "title": obj[i].title ,"text": obj[i].text };
     }
-  })
+});
 
+function updateJsonFile(savedNotes) {
+   jsonfile.writeFile(file, savedNotes, err => { 
+     
+    if (err) throw err;  
+       //console.log("Done writing"); // Success 
+    }); 
+};
+
+module.exports = { 
+    savedNotes: savedNotes,
+    updateJsonFile: updateJsonFile
+};

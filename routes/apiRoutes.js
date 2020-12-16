ab5925669d5015737773a351b5fffa658560c78e
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-12-16 11:04:34 -0800

    Discovered an error in the terminal when deleting notes.  Added return statements for res.json parts to prevent the errors.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ let store = require("../db/store");
 module.exports = function(app) {
     //For GET requests, will display current saved notes data
     app.get("/api/notes", function(req, res) {
-        res.json(store.savedNotes);
+        return res.json(store.savedNotes);
     });
 
     //for POST requests, will add the note to the savedNotes array, update the db.json file, and the current saved notes will be displayed
@@ -13,7 +13,7 @@ module.exports = function(app) {
         req.body.id = (store.savedNotes.length + 1).toString();
         store.savedNotes.push(req.body);
         store.updateJsonFile(store.savedNotes);
-        res.json(store.savedNotes);
+        return res.json(store.savedNotes);
     })
 
     //for DELETE requests, will remove the note being deleted based on the id of the note using the .filter method; the db.json will be updated, and the current notes will be displayed
@@ -25,7 +25,7 @@ module.exports = function(app) {
           if (chosen === store.savedNotes[i].id) {
             store.savedNotes = store.savedNotes.filter(({ id }) => id !== req.params.id);
             store.updateJsonFile(store.savedNotes);
-            res.json(store.savedNotes);
+            return res.json(store.savedNotes);
           }
         }
       
